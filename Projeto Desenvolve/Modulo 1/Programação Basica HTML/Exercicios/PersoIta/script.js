4b030f8e4b1e16a60754f7f442d4b8a24f035618
Williammateus18 <willianm1807@gmail.com>
2025-10-02 22:12:13 -0300

    Projeto Desenvolve - Modulo 1

---
+++ Projeto Desenvolve/Modulo 1/Programação Basica HTML/Exercicios/PersoIta/script.js
@@ -0,0 +1,196 @@
+window.onload = function() {
+    var dimensionsDiv = document.getElementById('dimensions');
+    dimensionsDiv.innerHTML = 'Width: ' + window.innerWidth + 'px, Height: ' + window.innerHeight + 'px';
+    // Atualiza as variáveis CSS com as dimensões da tela
+    document.documentElement.style.setProperty('--screen-width', window.innerWidth + 'px');
+    document.documentElement.style.setProperty('--screen-height', window.innerHeight + 'px');
+}
+function abrirModal (){
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal')
+        modal.classList.remove('abrir')
+    })
+}
+function abrirModal1 (){
+    const modal = document.getElementById('janela-modal1')
+    modal.classList.add('abrir1')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar1' || e.target.id == 'janela-modal1')
+        modal.classList.remove('abrir1')
+    })
+}
+function abrirModal2 (){
+    const modal = document.getElementById('janela-modal2')
+    modal.classList.add('abrir2')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar2' || e.target.id == 'janela-modal2')
+        modal.classList.remove('abrir2')
+    })
+}
+function abrirModal3 (){
+    const modal = document.getElementById('janela-modal3')
+    modal.classList.add('abrir3')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar3' || e.target.id == 'janela-modal3')
+        modal.classList.remove('abrir3')
+    })
+}
+function abrirModal4 (){
+    const modal = document.getElementById('janela-modal4')
+    modal.classList.add('abrir4')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar4' || e.target.id == 'janela-modal4')
+        modal.classList.remove('abrir4')
+    })
+}
+function abrirModal5 (){
+    const modal = document.getElementById('janela-modal5')
+    modal.classList.add('abrir5')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar5' || e.target.id == 'janela-modal5')
+        modal.classList.remove('abrir5')
+    })
+}
+function abrirModal6 (){
+    const modal = document.getElementById('janela-modal6')
+    modal.classList.add('abrir6')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar6' || e.target.id == 'janela-modal6')
+        modal.classList.remove('abrir6')
+    })
+}
+function abrirModal7 (){
+    const modal = document.getElementById('janela-modal7')
+    modal.classList.add('abrir7')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar7' || e.target.id == 'janela-modal7')
+        modal.classList.remove('abrir7')
+    })
+}
+function abrirModal8 (){
+    const modal = document.getElementById('janela-modal8')
+    modal.classList.add('abrir8')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar8' || e.target.id == 'janela-modal8')
+        modal.classList.remove('abrir8')
+    })
+}
+function abrirModal9 (){
+    const modal = document.getElementById('janela-modal9')
+    modal.classList.add('abrir9')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar9' || e.target.id == 'janela-modal9')
+        modal.classList.remove('abrir9')
+    })
+}
+function abrir1Modal (){
+    const modal = document.getElementById('janela-modal10')
+    modal.classList.add('abrir10')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar10' || e.target.id == 'janela-modal10')
+        modal.classList.remove('abrir10')
+    })
+}
+function abrir2Modal (){
+    const modal = document.getElementById('janela-modal11')
+    modal.classList.add('abrir11')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar11' || e.target.id == 'janela-modal11')
+        modal.classList.remove('abrir11')
+    })
+}
+function abrir3Modal (){
+    const modal = document.getElementById('janela-modal12')
+    modal.classList.add('abrir12')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar12' || e.target.id == 'janela-modal12')
+        modal.classList.remove('abrir12')
+    })
+}
+function abrir4Modal (){
+    const modal = document.getElementById('janela-modal13')
+    modal.classList.add('abrir13')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar13' || e.target.id == 'janela-modal13')
+        modal.classList.remove('abrir13')
+    })
+}
+function abrir5Modal (){
+    const modal = document.getElementById('janela-modal14')
+    modal.classList.add('abrir14')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar14' || e.target.id == 'janela-modal14')
+        modal.classList.remove('abrir14')
+    })
+}
+function abrir6Modal (){
+    const modal = document.getElementById('janela-modal15')
+    modal.classList.add('abrir15')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar15' || e.target.id == 'janela-modal15')
+        modal.classList.remove('abrir15')
+    })
+}
+function abrir7Modal (){
+    const modal = document.getElementById('janela-modal16')
+    modal.classList.add('abrir16')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar16' || e.target.id == 'janela-modal16')
+        modal.classList.remove('abrir16')
+    })
+}
+function abrir8Modal (){
+    const modal = document.getElementById('janela-modal17')
+    modal.classList.add('abrir17')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar17' || e.target.id == 'janela-modal17')
+        modal.classList.remove('abrir17')
+    })
+}
+function abrir9Modal (){
+    const modal = document.getElementById('janela-modal18')
+    modal.classList.add('abrir18')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar18' || e.target.id == 'janela-modal18')
+        modal.classList.remove('abrir18')
+    })
+}
+function abrir10Modal (){
+    const modal = document.getElementById('janela-modal19')
+    modal.classList.add('abrir19')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar19' || e.target.id == 'janela-modal19')
+        modal.classList.remove('abrir19')
+    })
+}
+function abrir11Modal (){
+    const modal = document.getElementById('janela-modal20')
+    modal.classList.add('abrir20')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar20' || e.target.id == 'janela-modal20')
+        modal.classList.remove('abrir20')
+    })
+}
